dd94243e6466ef90db97c6148306192da8736355
ADPM Drones <lab@adpmdrones.com>
2022-12-16 20:47:46 +0100

    create directories

--- cm4-test.py
+++ cm4-test.py
@@ -5,49 +5,53 @@ import time
 import requests
 import logging
 import json
-import math
 
 # Create logger
-logger = logging.getLogger('IRS')
-logging.basicConfig(level=logging.INFO, filename='/var/logIRS.log', \
+logger = logging.getLogger('CBFC')
+logging.basicConfig(level=logging.INFO, filename='/var/CBFC.log', \
 					format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', \
 					datefmt='%d/%m/%Y %I:%M:%S %p')
 
-# Device Token CM4 Test
+# Device Token CM4 Test - ThingsBoard ADPM
 device_token = "EeLqJHNQgWR4FtycieRD"
 
 # Drone ID
 droneID = "64"
 
+# Wait time between reads (seconds)
+wait_time = 0.5
+
 # Endpoints
+#
+# Mavlink2rest
 url_uav = "http://localhost:8088/mavlink/vehicles/" + droneID + "/components/1/messages"
+#
+# ThingsBoard ADPM
 url_dashboard = "http://dashboard.adpmdrones.com:8080/api/v1/" + device_token + "/telemetry"
 
 while True:
-	time.sleep(1.0)
+	time.sleep(wait_time)
 	try:
 		print("Start local request")
 		r = requests.get(url_uav , timeout=2)
 		data = r.json()
-		#print(data)
+
 		data_gps = data["GPS_RAW_INT"]["message"]
 		data_attitude = data["ATTITUDE"]["message"]
-		#print("==========")
 
-		ALT = data_gps["alt"]			#
-		LAT = data_gps["lat"]			#
-		LON = data_gps["lon"]			#
-		SAT = data_gps["satellites_visible"]	#
-		PIT = data_attitude["pitch"]		#
-		RLL = data_attitude["roll"]		#
-		YAW = data_attitude["yaw"]		#
-		VEL = data_gps["vel"]			# GPS ground speed cm/s
-
-		timestamp = data["ATTITUDE"]["status"]["time"]["last_update"]
+		TIS = data["ATTITUDE"]["status"]["time"]["last_update"]	# Timestamp
+		ALT = data_gps["alt"]									# Altitude  (MSL). Positive for up. mm
+		LAT = data_gps["lat"]									# Latitude  (WGS84, EGM96 ellipsoid) degE7
+		LON = data_gps["lon"]									# Longitude (WGS84, EGM96 ellipsoid) degE7
+		SAT = data_gps["satellites_visible"]					# Number of satellites visible
+		PIT = data_attitude["pitch"]							# Pitch angle (-pi..+pi) radians
+		RLL = data_attitude["roll"]								# Roll  angle (-pi..+pi) radians
+		YAW = data_attitude["yaw"]								# Yaw   angle (-pi..+pi) radians
+		VEL = data_gps["vel"]									# GPS ground speed cm/s
 
 		data = '{' + \
 			'\"droneid\":' + str(droneID) + \
-			',\"timestamp\":\"' + timestamp + '\"' + \
+			',\"timestamp\":\"' + TIS + '\"' + \
 			',\"latitude\":' + str(LAT / 10000000) + \
 			',\"longitude\":' + str(LON / 10000000) + \
 			',\"altitude\":' + str(ALT / 1000) + \
@@ -58,15 +62,15 @@ while True:
 			',\"yaw\":' + str(YAW) + \
 			'}'
 		data = json.loads(data)
+		logger.info(data)
 		print(data)
 
 		r = requests.post(url_dashboard, timeout=2, json=data)
-		print(r)
-
+		logger.info(r.status_code)
+		print(r.status_code)
 
 	except KeyboardInterrupt:
 		break
-
 	except:
 		logger.error("POST Issue")
 		pass
---
+++ mavlink/websocket_client.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import argparse
+import asyncio
+import aiohttp
+
+
+async def start_client(url: str) -> None:
+    ws = await aiohttp.ClientSession().ws_connect(url, autoclose=False, autoping=False)
+
+    async def dispatch():
+        while True:
+            msg = await ws.receive()
+
+            if msg.type == aiohttp.WSMsgType.TEXT:
+                print("Text: ", msg.data.strip())
+            elif msg.type == aiohttp.WSMsgType.BINARY:
+                print("Binary: ", msg.data)
+            elif msg.type == aiohttp.WSMsgType.PING:
+                await ws.pong()
+            elif msg.type == aiohttp.WSMsgType.PONG:
+                print("Pong received")
+            else:
+                if msg.type == aiohttp.WSMsgType.CLOSE:
+                    await ws.close()
+                elif msg.type == aiohttp.WSMsgType.ERROR:
+                    print("Error during receive %s" % ws.exception())
+                elif msg.type == aiohttp.WSMsgType.CLOSED:
+                    pass
+
+                break
+
+    await dispatch()
+
+
+ARGS = argparse.ArgumentParser(
+    description="websocket console client for wssrv.py example."
+)
+ARGS.add_argument(
+    "--filter",
+    action="store",
+    dest="filter",
+    default=".*",
+    help="Regex filter or message name used on websocket: ATTITUDE,HEARTBEAT,RAW_IMU",
+)
+ARGS.add_argument(
+    "--url",
+    action="store",
+    dest="url",
+    default="http://localhost:8088",
+    help="Websocket address, follow the format: http://0.0.0.0:8088",
+)
+
+if __name__ == "__main__":
+    args = ARGS.parse_args()
+
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(
+        start_client(
+            args.url + "/ws/mavlink" + f"?filter={args.filter}"
+        )
+    )
