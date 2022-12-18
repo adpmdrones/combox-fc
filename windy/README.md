8b468eada1d93902823894d0c97dd7a77757099e
ADPM Drones <lab@adpmdrones.com>
2022-12-18 17:09:47 +0100

    Windy API

---
+++ windy/README.md
@@ -0,0 +1,8 @@
+# combox-fc
+COMBOX with Flight Control Hardware
+
+```bash
+https://api.windy.com/point-forecast/docs
+```
+
+
---
+++ windy/windy_api.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import time
+import requests
+import logging
+import json
+
+
+# Create logger
+logger = logging.getLogger('WINDY')
+logging.basicConfig(level=logging.INFO, filename='/var/log/WINDY-data.log', \
+					format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', \
+					datefmt='%d/%m/%Y %I:%M:%S %p')
+
+# Windy API token
+windy_token = "F0qmICttsRDw0UQ2G7KGw6K9B7FHngEY"
+
+
+latitude = 
+longitude =
+altitude =
+
+# Wait time between reads (seconds)
+wait_time = 0.25
+
+# Endpoints
+#
+# Mavlink2rest
+url_windy = "https://api.windy.com/api/point-forecast/v2"
+
+data = {
+    "lat": 49.809,
+    "lon": 16.787,
+    "model": "gfs",
+    "parameters": ["wind", "dewpoint", "rh", "pressure"],
+    "levels": ["surface", "800h", "300h"],
+    "key": "F0qmICttsRDw0UQ2G7KGw6K9B7FHngEY"
+}
+
+while True:
+	time.sleep(wait_time)
+    print(f"Start local request to {url_windy}")
+    header = {"Content-Type" :"application/json"}
+    r = requests.post(url_windy , headers = header, json = data)
+    print(r.text)
