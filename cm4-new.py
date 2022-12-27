81aa9ddbe7060fd2e74ae71624f3b64917394645
ADPM Drones <lab@adpmdrones.com>
2022-12-27 11:11:40 +0100

    decimals

--- cm4-new.py
+++ cm4-new.py
@@ -219,8 +219,8 @@ while True:
 	telem.vfr_heading = data_vfr["heading"]							# Current heading in compass units (0-360, 0=north) Deg
 
 	data_pressure = data["SCALED_PRESSURE"]["message"] 				# https://mavlink.io/en/messages/common.html#SCALED_PRESSURE
-	telem.press_abs = float("{:.2f}".format(data_pressure["press_abs"]))	# Absolute pressure hPa
-	telem.press_dif = data_pressure["press_diff"]					# Differential pressure hPa
+	telem.press_abs = float("{:.4f}".format(data_pressure["press_abs"]))	# Absolute pressure hPa
+	telem.press_dif = float("{:.4f}".format(data_pressure["press_diff"]))	# Differential pressure hPa
 	telem.press_tmp = data_pressure["temperature"] / 100			# Absolute pressure temperature cdegC
 
 	# jsonTelem = json.dumps(telem.__dict__)
