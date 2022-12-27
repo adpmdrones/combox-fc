5e7b6da364f3b55e6621bc7a5e18a17a2cd6f1a2
ADPM Drones <lab@adpmdrones.com>
2022-12-27 13:34:06 +0100

    update adsb data

--- cm4-new.py
+++ cm4-new.py
@@ -13,6 +13,8 @@
 # variables for the drone and dashboard. 
 # The script then makes periodic requests to the drone for MAVLink data and sends telemetry data to the dashboard via HTTP POST requests.
 
+# In order to avoid incorrect behavior when working with the map, we use different devices to show their coordinates on the map
+# for tracking vehicle and ADSB data
 
 import time
 import requests
@@ -49,8 +51,8 @@ url_vehicle = "http://localhost:8088/mavlink/vehicles/"
 
 #
 # ThingsBoard ADPM
-url_dashboard = f"http://dashboard.adpmdrones.com:8080/api/v1/{device_token}/telemetry"
-url_adsb = f"http://dashboard.adpmdrones.com:8080/api/v1/{device_token_ADSB}/telemetry"
+url_device = f"http://dashboard.adpmdrones.com:8080/api/v1/{device_token}/telemetry"
+url_device_adsb = f"http://dashboard.adpmdrones.com:8080/api/v1/{device_token_ADSB}/telemetry"
 
 # Reading mavlink stream
 def read_mavlink(droneID, url_uav):
@@ -96,7 +98,7 @@ class telemetry_adsb:
 	droneid = 1				# DroneID
 	timestamp = ''			# timestamp
 
-
+# We try from 1 to 255 to get automatically the mavid
 # Finding mav id
 for n in range (255):
 	# Request Mavlink data
@@ -136,7 +138,7 @@ autopilot = data["HEARTBEAT"]["message"]["autopilot"]["type"]		# https://mavlink
 mavtype_multirotor = ["MAV_TYPE_QUADROTOR", "MAV_TYPE_HEXAROTOR", "MAV_TYPE_OCTOROTOR"]
 mavtype_wing = ["MAV_TYPE_FIXED_WING"]
 
-mavtype = data["HEARTBEAT"]["message"]["mavtype"]["type"]			# https://mavlink.io/en/messages/common.html#MAV_TYPE
+mavtype = data["HEARTBEAT"]["message"]["mavtype"]["type"]				# https://mavlink.io/en/messages/common.html#MAV_TYPE
 
 if (autopilot == "MAV_AUTOPILOT_ARDUPILOTMEGA"):
 	print(autopilot)
@@ -176,26 +178,30 @@ while True:
 	# Check if ADSB data is available
 	if "ADSB_VEHICLE" in data:
 		# Read ADSB data
-		data_adsb = data["ADSB_VEHICLE"]["message"]					# https://mavlink.io/en/messages/common.html#ADSB_VEHICLE
+		data_adsb = data["ADSB_VEHICLE"]["message"]							# https://mavlink.io/en/messages/common.html#ADSB_VEHICLE
 		# Update
-		telem.adsb_lat = data_adsb["lat"] / 10000000				# Latitude  (WGS84, EGM96 ellipsoid) degE7
-		telem.adsb_lon = data_adsb["lon"] / 10000000				# Longitude (WGS84, EGM96 ellipsoid) degE7
-		telem.adsb_heading = data_adsb["heading"] / 100				# Course over ground cDeg
-		telem.adsb_hor_velocity = data_adsb["hor_velocity"] /100 	# The horizontal velocity cm/s
-		telem.adsb_ver_velocity = data_adsb["ver_velocity"] / 100	# The vertical velocity. Positive is up
-		telem.adsb_squawk = data_adsb["squawk"]						# Squawk code
-		telem.adsb_type = data_adsb["type"]							# 
-		telem.adsb_tslc = data_adsb["tslc"]							# Time since last communication in seconds
+		telem.adsb_lat = data_adsb["lat"] / 10000000						# Latitude  (WGS84, EGM96 ellipsoid) degE7
+		telem.adsb_lon = data_adsb["lon"] / 10000000						# Longitude (WGS84, EGM96 ellipsoid) degE7
+		telem.adsb_heading = data_adsb["heading"] / 100						# Course over ground cDeg
+		telem.adsb_hor_velocity = data_adsb["hor_velocity"] /100 			# The horizontal velocity cm/s
+		telem.adsb_ver_velocity = data_adsb["ver_velocity"] / 100			# The vertical velocity. Positive is up
+		telem.adsb_squawk = data_adsb["squawk"]								# Squawk code
+		telem.adsb_type = data_adsb["type"]									# 
+		telem.adsb_tslc = data_adsb["tslc"]									# Time since last communication in seconds
 		# Update
-		adsb.adsb_lat = data_adsb["lat"] / 10000000					# Latitude  (WGS84, EGM96 ellipsoid) degE7
-		adsb.adsb_lon = data_adsb["lon"] / 10000000					# Longitude (WGS84, EGM96 ellipsoid) degE7
-		adsb.adsb_heading = data_adsb["heading"] / 100				# Course over ground cDeg
-		adsb.adsb_hor_velocity = data_adsb["hor_velocity"] /100 	# The horizontal velocity cm/s
-		adsb.adsb_ver_velocity = data_adsb["ver_velocity"] / 100	# The vertical velocity. Positive is up
-		adsb.adsb_squawk = data_adsb["squawk"]						# Squawk code
-		adsb.adsb_type = data_adsb["type"]							# 
-		adsb.adsb_tslc = data_adsb["tslc"]							# Time since last communication in seconds
-
+		adsb.adsb_lat = data_adsb["lat"] / 10000000							# Latitude  (WGS84, EGM96 ellipsoid) degE7
+		adsb.adsb_lon = data_adsb["lon"] / 10000000							# Longitude (WGS84, EGM96 ellipsoid) degE7
+		adsb.adsb_heading = data_adsb["heading"] / 100						# Course over ground cDeg
+		adsb.adsb_hor_velocity = data_adsb["hor_velocity"] /100 			# The horizontal velocity cm/s
+		adsb.adsb_ver_velocity = data_adsb["ver_velocity"] / 100			# The vertical velocity. Positive is up
+		adsb.adsb_squawk = data_adsb["squawk"]								# Squawk code
+		adsb.adsb_type = data_adsb["type"]									# 
+		adsb.adsb_tslc = data_adsb["tslc"]									# Time since last communication in seconds
+		adsb.adsb_icao = data_adsb["ICAO_address"]
+		adsb.adsb_alt = data_adsb["altitude"]
+		adsb.adsb_alt = data_adsb["altitude_type"]["type"]
+		adsb.adsb_alt = data_adsb["emitter_type"]["type"]
+		
 	#
 	# Check if WIND data is available
 	if "WIND" in data:
@@ -206,21 +212,21 @@ while True:
 		telem.wind_speed = data_wind["speed"]
 		telem.wind_speed_z = data_wind["speed_z"]
 	#
-	data_gps_raw_int = data["GPS_RAW_INT"]["message"]				# https://mavlink.io/en/messages/common.html#GPS_RAW_INT
-	telem.vel = data_gps_raw_int["vel"] / 100						# GPS ground speed cm/s
-	telem.satellites = data_gps_raw_int["satellites_visible"]		# Number of satellites visible
+	data_gps_raw_int = data["GPS_RAW_INT"]["message"]						# https://mavlink.io/en/messages/common.html#GPS_RAW_INT
+	telem.vel = data_gps_raw_int["vel"] / 100								# GPS ground speed cm/s
+	telem.satellites = data_gps_raw_int["satellites_visible"]				# Number of satellites visible
 	#
-	data_gps_int = data["GLOBAL_POSITION_INT"]["message"]			# https://mavlink.io/en/messages/common.html#GLOBAL_POSITION_INT
-	telem.altitude_msl = data_gps_int["alt"] / 1000					# Altitude  (MSL). Positive for up. mm
-	telem.altitude = data_gps_int["relative_alt"] / 1000			# Altitude above ground
-	telem.latitude = data_gps_int["lat"] / 10000000					# Latitude  (WGS84, EGM96 ellipsoid) degE7
-	telem.longitude = data_gps_int["lon"] / 10000000				# Longitude (WGS84, EGM96 ellipsoid) degE7
-	telem.heading = data_gps_int["hdg"]								# Vehicle heading (yaw angle), 0.0..359.99 degrees
+	data_gps_int = data["GLOBAL_POSITION_INT"]["message"]					# https://mavlink.io/en/messages/common.html#GLOBAL_POSITION_INT
+	telem.altitude_msl = data_gps_int["alt"] / 1000							# Altitude  (MSL). Positive for up. mm
+	telem.altitude = data_gps_int["relative_alt"] / 1000					# Altitude above ground
+	telem.latitude = data_gps_int["lat"] / 10000000							# Latitude  (WGS84, EGM96 ellipsoid) degE7
+	telem.longitude = data_gps_int["lon"] / 10000000						# Longitude (WGS84, EGM96 ellipsoid) degE7
+	telem.heading = data_gps_int["hdg"]										# Vehicle heading (yaw angle), 0.0..359.99 degrees
 	telem.vx = float("{:.4f}".format(data_gps_int["vx"] / 100))								# Ground X Speed (Latitude, positive north) cm/s
 	telem.vy = float("{:.4f}".format(data_gps_int["vy"] / 100))								# Ground Y Speed (Longitude, positive east) cm/s
 	telem.vz = float("{:.4f}".format(data_gps_int["vz"] / 100))								# Ground Z Speed (Altitude, positive down) cm/s
 	#
-	data_attitude = data["ATTITUDE"]["message"]						# https://mavlink.io/en/messages/common.html#ATTITUDE
+	data_attitude = data["ATTITUDE"]["message"]												# https://mavlink.io/en/messages/common.html#ATTITUDE
 	telem.pitch = float("{:.4f}".format(math.degrees(data_attitude["pitch"])))				# Pitch angle (-pi..+pi) radians
 	telem.roll = float("{:.4f}".format(math.degrees(data_attitude["roll"])))				# Roll  angle (-pi..+pi) radians
 	telem.yaw = float("{:.4f}".format(math.degrees(data_attitude["yaw"])))					# Yaw   angle (-pi..+pi) radians
@@ -228,59 +234,57 @@ while True:
 	telem.roll_speed = float("{:.4f}".format(data_attitude["rollspeed"]))					# Ground Y Speed (Longitude, positive east) cm/s
 	telem.yaw_speed = float("{:.4f}".format(data_attitude["yawspeed"]))						# Ground Z Speed (Altitude, positive down) cm/s
 	#
-	data_vibration = data["VIBRATION"]["message"]					# https://mavlink.io/en/messages/common.html#VIBRATION 
+	data_vibration = data["VIBRATION"]["message"]											# https://mavlink.io/en/messages/common.html#VIBRATION 
 	telem.vibration_x = float("{:.4f}".format(data_vibration["vibration_x"]))				# Vibration levels on X-axis
 	telem.vibration_y = float("{:.4f}".format(data_vibration["vibration_y"]))				# Vibration levels on Y-axis
 	telem.vibration_z = float("{:.4f}".format(data_vibration["vibration_z"]))				# Vibration levels on Z-axis
 	#
-	data_servo = data["SERVO_OUTPUT_RAW"]["message"]				# https://mavlink.io/en/messages/common.html#SERVO_OUTPUT_RAW
-	telem.servo1 = data_servo["servo1_raw"]							# Value 900-2100 ms (DO NOT USE- ASSIGNED TO FC controls)
-	telem.servo2 = data_servo["servo2_raw"]							# Value 900-2100 ms (DO NOT USE- ASSIGNED TO FC controls)
-	telem.servo3 = data_servo["servo3_raw"]							# Value 900-2100 ms (DO NOT USE- ASSIGNED TO FC controls)
-	telem.servo4 = data_servo["servo4_raw"]							# Value 900-2100 ms (DO NOT USE- ASSIGNED TO FC controls)
-	telem.servo5 = data_servo["servo5_raw"]							# Value 900-2100 ms
-	telem.servo6 = data_servo["servo6_raw"]							# Value 900-2100 ms
-	telem.servo7 = data_servo["servo7_raw"]							# Value 900-2100 ms
-	telem.servo8 = data_servo["servo8_raw"]							# Value 900-2100 ms
-	telem.servo9 = data_servo["servo9_raw"]							# Value 900-2100 ms
-	telem.servo10 = data_servo["servo10_raw"]						# Value 900-2100 ms
-	telem.servo11 = data_servo["servo11_raw"]						# Value 900-2100 ms
-	telem.servo12 = data_servo["servo12_raw"]						# Value 900-2100 ms
-	telem.servo13 = data_servo["servo13_raw"]						# Value 900-2100 ms
-	telem.servo14 = data_servo["servo14_raw"]						# Value 900-2100 ms
-	telem.servo15 = data_servo["servo15_raw"]						# Value 900-2100 ms
-	telem.servo16 = data_servo["servo16_raw"]						# Value 900-2100 ms
+	data_servo = data["SERVO_OUTPUT_RAW"]["message"]						# https://mavlink.io/en/messages/common.html#SERVO_OUTPUT_RAW
+	telem.servo1 = data_servo["servo1_raw"]									# Value 900-2100 ms (DO NOT USE- ASSIGNED TO FC controls)
+	telem.servo2 = data_servo["servo2_raw"]									# Value 900-2100 ms (DO NOT USE- ASSIGNED TO FC controls)
+	telem.servo3 = data_servo["servo3_raw"]									# Value 900-2100 ms (DO NOT USE- ASSIGNED TO FC controls)
+	telem.servo4 = data_servo["servo4_raw"]									# Value 900-2100 ms (DO NOT USE- ASSIGNED TO FC controls)
+	telem.servo5 = data_servo["servo5_raw"]									# Value 900-2100 ms
+	telem.servo6 = data_servo["servo6_raw"]									# Value 900-2100 ms
+	telem.servo7 = data_servo["servo7_raw"]									# Value 900-2100 ms
+	telem.servo8 = data_servo["servo8_raw"]									# Value 900-2100 ms
+	telem.servo9 = data_servo["servo9_raw"]									# Value 900-2100 ms
+	telem.servo10 = data_servo["servo10_raw"]								# Value 900-2100 ms
+	telem.servo11 = data_servo["servo11_raw"]								# Value 900-2100 ms
+	telem.servo12 = data_servo["servo12_raw"]								# Value 900-2100 ms
+	telem.servo13 = data_servo["servo13_raw"]								# Value 900-2100 ms
+	telem.servo14 = data_servo["servo14_raw"]								# Value 900-2100 ms
+	telem.servo15 = data_servo["servo15_raw"]								# Value 900-2100 ms
+	telem.servo16 = data_servo["servo16_raw"]								# Value 900-2100 ms
 	#
-	data_vfr = data["VFR_HUD"]["message"]							# https://mavlink.io/en/messages/common.html#VFR_HUD
-	telem.vfr_airspeed = data_vfr["airspeed"]						# Vehicle speed in form appropriate for vehicle type.
-																	# For standard aircraft this is typically calibrated airspeed (CAS)
-																	# or indicated airspeed (IAS) - either of which can be used by a pilot
-																	# to estimate stall speed. m/s
-	telem.vfr_alt = data_vfr["alt"]									# Current altitude (MSL) m
-	telem.vfr_climb = data_vfr["climb"]								# Current climb rate m/s
-	telem.vfr_speed = data_vfr["groundspeed"]						# Current ground speed m/s
-	telem.vfr_heading = data_vfr["heading"]							# Current heading in compass units (0-360, 0=north) Deg
+	data_vfr = data["VFR_HUD"]["message"]									# https://mavlink.io/en/messages/common.html#VFR_HUD
+	telem.vfr_airspeed = data_vfr["airspeed"]								# Vehicle speed in form appropriate for vehicle type.
+																			# For standard aircraft this is typically calibrated airspeed (CAS)
+																			# or indicated airspeed (IAS) - either of which can be used by a pilot
+																			# to estimate stall speed. m/s
+	telem.vfr_alt = data_vfr["alt"]											# Current altitude (MSL) m
+	telem.vfr_climb = data_vfr["climb"]										# Current climb rate m/s
+	telem.vfr_speed = data_vfr["groundspeed"]								# Current ground speed m/s
+	telem.vfr_heading = data_vfr["heading"]									# Current heading in compass units (0-360, 0=north) Deg
 
-	data_pressure = data["SCALED_PRESSURE"]["message"] 				# https://mavlink.io/en/messages/common.html#SCALED_PRESSURE
+	data_pressure = data["SCALED_PRESSURE"]["message"] 						# https://mavlink.io/en/messages/common.html#SCALED_PRESSURE
 	telem.press_abs = float("{:.4f}".format(data_pressure["press_abs"]))	# Absolute pressure hPa
 	telem.press_dif = float("{:.4f}".format(data_pressure["press_diff"]))	# Differential pressure hPa
-	telem.press_tmp = data_pressure["temperature"] / 100			# Absolute pressure temperature cdegC
+	telem.press_tmp = data_pressure["temperature"] / 100					# Absolute pressure temperature cdegC
 
 	# jsonTelem = json.dumps(telem.__dict__)
 	# jsonTelem will be dumped in write_telemetry
 
 	jsonTelem = (telem.__dict__)
 	print(jsonTelem)
-	print(url_dashboard)
-
-	write_telemetry(jsonTelem, url_dashboard)
+	print(url_device)
+	write_telemetry(jsonTelem, url_device)
 
 	if "ADSB_VEHICLE" in data:
 		jsonADSB = (adsb.__dict__)
 		print(jsonADSB)
-		print(url_adsb)
-
-		write_telemetry(jsonADSB, url_adsb)
+		print(url_device_adsb)
+		write_telemetry(jsonADSB, url_device_adsb)
 
 	# Wait for next read
 	time.sleep(wait_time)
