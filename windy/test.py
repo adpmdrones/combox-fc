be496cd04d16f33539e24b17d75ed9f9d82fb9d3
ADPM Drones <lab@adpmdrones.com>
2023-01-06 19:20:32 +0100

    upd

--- windy/test.py
+++ windy/test.py
@@ -63,15 +63,13 @@ windy_data = get_windy(jsonWindy, windy_url, 41, 12)
 
 windy_forecast_span = windy_forecast * 3 * 3600	# transform in seconds
 
-for n in range (16):
+for n in range (6):
 
 	dt_windy_obj = datetime.fromtimestamp(windy_data["ts"][n]/1000.0)
 	dt_windy = int(float(dt_windy_obj.strftime('%s.%f')))
-	print("Windy ts ", dt_windy, datetime.fromtimestamp(dt_windy))
 
 	dt_obj = datetime.utcnow() 
 	dt = int(float(dt_obj.strftime('%s.%f')))
-	print("Box ts ", dt)
 
 	if dt_windy >= dt and dt_windy <= dt + windy_forecast_span:
 		os.system('clear')
