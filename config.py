d94e8a45735aac10e8c76e8c915a81152a1e1d9e
ADPM Drones <lab@adpmdrones.com>
2023-01-07 11:38:41 +0100

    upd

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def config(self):
+class config(self):
     ###########################
     # Configuration variables #
     ###########################
