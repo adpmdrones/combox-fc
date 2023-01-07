c2b038e82c3a6dfe3f229322bcbb193ef241a957
ADPM Drones <lab@adpmdrones.com>
2023-01-07 12:00:31 +0100

    upd

--- cm4-new_v2.py
+++ cm4-new_v2.py
@@ -34,35 +34,43 @@ logging.basicConfig(level=logging.INFO, filename='/var/log/CBFC-data.log', \
 # Configuration variables #
 ###########################
 class config(object):
-		def config(self):
-			###########################
-			# Configuration variables #
-			###########################
-			# Device UUID version 4
-			combox_UUID = "331f20a2-a36d-4a2a-add1-56dcb1757b5d"
+	###########################
+	# Configuration variables #
+	###########################
+	# Device UUID version 4
+	def combox_UUID(self):
+		combox_UUID = "331f20a2-a36d-4a2a-add1-56dcb1757b5d"
 
-			# Device Token CM4 Test - ThingsBoard ADPM
-			device_token = "EeLqJHNQgWR4FtycieRD"
-			device_token_ADSB = "ucaEaGMnN491sfbyBP1g"
+	# Device Token CM4 Test - ThingsBoard ADPM
+	def token(self):
+		device_token = "EeLqJHNQgWR4FtycieRD"
+		device_token_ADSB = "ucaEaGMnN491sfbyBP1g"
 
-            # Windy Token
-			windy_token = "F0qmICttsRDw0UQ2G7KGw6K9B7FHngEY"
-			windy_url = "https://api.windy.com/api/point-forecast/v2"
+	# Windy Token
+	def windy(self):
+		windy_token = "F0qmICttsRDw0UQ2G7KGw6K9B7FHngEY"
+		windy_url = "https://api.windy.com/api/point-forecast/v2"
 
-			# Drone ID - just for setting the variable
-			droneID = "1"
+	# Drone ID - just for setting the variable
+	def droneID(self):
+		droneID = "1"
 
-            # Wait time between reads (seconds)
-			wait_time = 5.0
+	# Wait time between reads (seconds)
+	def wait_time(self):
+		wait_time = 5.0
 
-            # Endpoints
-            #
-            # Mavlink2rest
-			url_uav = f"http://localhost:8088/mavlink/vehicles/{droneID}/components/1/messages" # url_uav - just for setting the variableurl_vehicle = "http://localhost:8088/mavlink/vehicles/"
-			url_vehicle = "http://localhost:8088/mavlink/vehicles/"
-            # ThingsBoard ADPM
-			url_device = f"http://dashboard.adpmdrones.com:8080/api/v1/{device_token}/telemetry"
-			url_device_adsb = f"http://dashboard.adpmdrones.com:8080/api/v1/{device_token_ADSB}/telemetry"
+	# Endpoints
+	#
+	# Mavlink2rest
+	def url_uav(self):
+		url_uav = f"http://localhost:8088/mavlink/vehicles/{droneID}/components/1/messages" # url_uav - just for setting the variableurl_vehicle = "http://localhost:8088/mavlink/vehicles/"
+	def url_vehicle(self):	
+		url_vehicle = "http://localhost:8088/mavlink/vehicles/"
+	# ThingsBoard ADPM
+	def url_device(self):
+		url_device = f"http://dashboard.adpmdrones.com:8080/api/v1/{device_token}/telemetry"
+	def url_device_adsb(self):
+		url_device_adsb = f"http://dashboard.adpmdrones.com:8080/api/v1/{device_token_ADSB}/telemetry"
 
 
 ###########################
@@ -117,7 +125,7 @@ def find_mavID():
 		# vehicle ID
 		for n in range (255):
 			# Request Mavlink data
-			url = config.url_vehicle() + str(n)
+			url = config.url_vehicle + str(n)
 			print("Checking vehicle ID " + str(n))
 			try:
 				r = requests.get(url, timeout=2)
@@ -203,7 +211,7 @@ adsb_list = []
 
 # Read config variables
 #
-config = config.config(self)
+config = config()
 
 # Finding mavID from the telemetry stream
 #
@@ -235,7 +243,7 @@ while True:
 	telem.timestamp = data["ATTITUDE"]["status"]["time"]["last_update"]
 
 	telem.droneid = droneID
-	telem.combox_UUID = config.combox_UUID()
+	telem.combox_UUID = config.combox_UUID
 	telem.autopilot = autopilot
 	telem.mavtype = mavtype
 
@@ -353,7 +361,7 @@ while True:
 	jsonTelem = (telem.__dict__)
 	print(jsonTelem)
 	print(config.url_device())
-	write_telemetry(jsonTelem, config.url_device())
+	write_telemetry(jsonTelem, config.url())
 
 	# ADSB data
 	#
