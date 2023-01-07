6c7f4237895b9ee2169ad6d74c920aca3da00857
ADPM Drones <lab@adpmdrones.com>
2023-01-07 12:21:38 +0100

    upd

--- cm4-new_v2.py
+++ cm4-new_v2.py
@@ -30,45 +30,8 @@ logging.basicConfig(level=logging.INFO, filename='/var/log/CBFC-data.log', \
 					format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', \
 					datefmt='%d/%m/%Y %I:%M:%S %p')
 
-###########################
-# Configuration variables #
-###########################
-class config():
 
-	# Device UUID version 4
-	#
-	combox_UUID = "331f20a2-a36d-4a2a-add1-56dcb1757b5d"
-
-	# Device Token CM4 Test - ThingsBoard ADPM
-	#
-	device_token = "EeLqJHNQgWR4FtycieRD"
-	device_token_ADSB = "ucaEaGMnN491sfbyBP1g"
-
-	# Windy Token
-	#
-	windy_token = "F0qmICttsRDw0UQ2G7KGw6K9B7FHngEY"
-	windy_url = "https://api.windy.com/api/point-forecast/v2"
-
-	# Drone ID - just for setting the variable
-	#
-	droneID = "1"
-
-	# Wait time between reads (seconds)
-	#
-	wait_time = 5.0
-
-	# Endpoints
-	#
-	# Mavlink2rest
-	#
-	url_uav = f"http://localhost:8088/mavlink/vehicles/{droneID}/components/1/messages" # url_uav - just for setting the variableurl_vehicle = "http://localhost:8088/mavlink/vehicles/"
-	url_vehicle = "http://localhost:8088/mavlink/vehicles/"
-
-	# ThingsBoard ADPM
-	#
-	url_device = f"http://dashboard.adpmdrones.com:8080/api/v1/{device_token}/telemetry"
-	url_device_adsb = f"http://dashboard.adpmdrones.com:8080/api/v1/{device_token_ADSB}/telemetry"
-
+import config
 
 ###########################
 #        Functions        #
