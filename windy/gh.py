ab6987425e68f959ca83b8903364f4dfb7dd0af3
adpmdrones <lab@adpmdrones.com>
2023-01-07 13:45:02 +0000

    test windy

--- windy/gh.py
+++ windy/gh.py
@@ -1,8 +1,15 @@
+import bisect
+
 # Gh altitude
 #
-windy_gh = [1000, 950, 925, 900, 850, 800, 700, 600, 500, 400, 300, 200, 150]
+#windy_gh = [1000, 950, 925, 900, 850, 800, 700, 600, 500, 400, 300, 200, 150]
+windy_gh = [150, 200, 300, 400, 500, 600, 700, 800, 850, 900, 925, 950, 1000]
 
-press = 1018.20
 
-gh = [ i for i in windy_gh if (press - i <= 25) or (press -i >= 25)]
+press = 927
+print(press)
+
+#gh = [ i for i in windy_gh if (press <= i and press >= i)]
+gh = bisect.bisect(windy_gh, press)
 print(gh)
+print(windy_gh[gh - 13])
