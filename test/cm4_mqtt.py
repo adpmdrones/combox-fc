66fe378d063dede45dc7281ae1c7cdf4de59fd43
ADPM Drones <lab@adpmdrones.com>
2023-01-08 16:13:58 +0100

    upd

--- test/cm4_mqtt.py
+++ test/cm4_mqtt.py
@@ -29,6 +29,7 @@ def on_connect(client, userdata, rc, *extra_params):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
+    print("*" * 20)
     print('Topic: ' + msg.topic + '\nMessage: ' + str(msg.payload))
     # Decode JSON request
     data = json.loads(msg.payload)
