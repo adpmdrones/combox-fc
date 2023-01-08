e5a86228998a925862bd153c9b2adb20c7f8053f
ADPM Drones <lab@adpmdrones.com>
2023-01-08 17:34:34 +0100

    upd

--- test/cm4_mqtt.py
+++ test/cm4_mqtt.py
@@ -16,8 +16,6 @@ THINGSBOARD_HOST = 'dashboard.adpmdrones.com'
 API = "http://localhost:8088"
 ACCESS_TOKEN = 'EeLqJHNQgWR4FtycieRD'
 
-servo_state = {1: 25}
-
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, rc, *extra_params):
     print('Connected with result code ' + str(rc))
@@ -46,7 +44,7 @@ def on_message(client, userdata, msg):
 
 def get_Value():
     print("Get Value")
-    servo_state[1] = random.randrange(0,100, step=10)
+    servo_state = random.randrange(0,100, step=10)
     print(servo_state)
     return servo_state
 
