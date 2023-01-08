20e2155f67b49256014e08453a5294d5864d0c33
ADPM Drones <lab@adpmdrones.com>
2023-01-08 15:35:44 +0100

    upd

--- test/cm4_mqtt.py
+++ test/cm4_mqtt.py
@@ -35,15 +35,16 @@ def on_message(client, userdata, msg):
     # Check request method
     if data['method'] == 'getValue':
         # Reply with status
-        client.publish(msg.topic.replace('request', 'response'), getValue(), 1)
+        #client.publish(msg.topic.replace('request', 'response'), getValue(), 1)
+        print("getValue dummy")
     elif data['method'] == 'setValue':
         # Update status and reply
         print(data)
         print(data['params'])
         print('response')
-        setValue(1000, data['params'])
-        client.publish(msg.topic.replace('request', 'response'), getValue(), 1)
-        client.publish('v1/devices/me/attributes', getValue(), 1)
+        #setValue(5, data['params'])
+        #client.publish(msg.topic.replace('request', 'response'), getValue(), 1)
+        #client.publish('v1/devices/me/attributes', getValue(), 1)
 
 def getValue():
     print("Get Value")
