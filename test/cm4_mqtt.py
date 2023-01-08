effe073b33d0ef00b8602c2cff5af4ce164895be
ADPM Drones <lab@adpmdrones.com>
2023-01-08 14:14:52 +0100

    upd

---
+++ test/cm4_mqtt.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+##########
+# by ADPM Drones Srl 2021-2023
+##########
+#
+
+import paho.mqtt.client as mqtt
+import requests
+import json
+
+THINGSBOARD_HOST = 'dashboard.adpmdrones.com'
+API = "http://localhost:8088"
+ACCESS_TOKEN = 'EeLqJHNQgWR4FtycieRD'
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, rc, *extra_params):
+    print('Connected with result code ' + str(rc))
+    # Subscribing to receive RPC requests
+    client.subscribe('v1/devices/me/rpc/request/+')
+    # Sending current GPIO status
+    client.publish('v1/devices/me/attributes', get_gpio_status(), 1)
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print('Topic: ' + msg.topic + '\nMessage: ' + str(msg.payload))
+    # Decode JSON request
+    data = json.loads(msg.payload)
+    # Check request method
+    if data['method'] == 'getGpioStatus':
+        # Reply with GPIO status
+        client.publish(msg.topic.replace('request', 'response'), get_gpio_status(), 1)
+    elif data['method'] == 'setGpioStatus':
+        # Update GPIO status and reply
+        set_gpio_status(data['params']['pin'], data['params']['enabled'])
+        client.publish(msg.topic.replace('request', 'response'), get_gpio_status(), 1)
+        client.publish('v1/devices/me/attributes', get_gpio_status(), 1)
+
+def getValue():
+    print("Get Value")
+
+def setValue():
+    print("Set Value")
+
+
+client = mqtt.Client()
+# Register connect callback
+client.on_connect = on_connect
+# Registed publish message callback
+client.on_message = on_message
+# Set access token
+client.username_pw_set(ACCESS_TOKEN)
+# Connect to ThingsBoard using default MQTT port and 60 seconds keepalive interval
+client.connect(THINGSBOARD_HOST, 1883, 60)
+
+try:
+    client.loop_forever()
+except KeyboardInterrupt:
+    break
+
+
+
