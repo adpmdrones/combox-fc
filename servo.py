dba745dcf84a58387483bcd110249689a75768bb
ADPM Drones <lab@adpmdrones.com>
2023-01-08 11:56:24 +0100

    upd

--- servo.py
+++ servo.py
@@ -10,7 +10,7 @@ def get(url_device, url_drone, servo_num, servo_pos):
 
 
 
-def set(url_device, url_drone, servo_num, position):
+def set(url_device, url_drone, servo_num, servo_pos):
     print("Set servo position")
     print(url_device)
     print(url_drone)
